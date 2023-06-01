c330eb82e4b751e26ce342dd0875d03c815f940f
pedro Torres <ped.torres@duocuc.cl>
2023-06-01 11:59:43 -0400

    numpy

---
+++ arreglos de numeros/areglos de una dimecion.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+listas=[1,2,3,4,5,6,7,8,9,0]
+arr=np.array(listas)
+print(arr)
---
+++ arreglos de numeros/ke.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+# Ejemplo de uso de numpy
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
