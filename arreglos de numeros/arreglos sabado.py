21c98fb0bf88590da3de8a6a0a43c4c3fb20a2b9
pedro Torres <ped.torres@duocuc.cl>
2023-06-08 19:09:20 -0400

    arreglos unidimencionales

--- arreglos de numeros/arreglos sabado.py
+++ arreglos de numeros/arreglos sabado.py
@@ -1,25 +1,29 @@
 import numpy as np
 import random 
 
-arreglo1 = np.random.randint(500, size=(100))
+arregloA = np.random.randint(500, size=(100))
 lista = []
 
-print("Los números aleatorios son: ", arreglo1)
+print("Los números aleatorios son: ", arregloA)
 
-for x in range(len(arreglo1)):
+for x in range(len(arregloA)):
     if x % 2 == 0:
-        lista.append(arreglo1[x])
+        lista.append(arregloA[x])
 
 print("Los números en los índices pares son: ", lista)
 
-arreglo_cop = arreglo1.copy()
+arreglo_cop = arregloA.copy()
+arreglo_cop1 = arregloA.copy()
 arreglo_cop.sort()
+
 
 numero = arreglo_cop[-1]
 
 print("El número mínimo es:", arreglo_cop[0])
 print("El número máximo es:", arreglo_cop[-1])
 
-indice = np.where(arreglo1 == numero)[0][0]
+indice = np.where(arregloA == numero)[0][0]
 print("El índice del número máximo en arreglo1 es:", indice)
-
+arreglo_cop1=arreglo_cop1*3
+print("hola",arreglo_cop1)
+np.sum(arreglo_cop1)
